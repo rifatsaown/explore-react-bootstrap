9fa176d4fbf3e2a1d68965c289bebc6e12abae66
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-01-31 02:38:37 +0600

    React-bootstarp installl

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Button } from 'react-bootstrap';
 import './App.css';
+import ControlledCarousel from './Carousel';
+
 
 function App() {
   return (
     <div className="App">
      <Button variant="danger">Primary</Button>
-     
+     <ControlledCarousel/>
     </div>
   );
 }
---
+++ src/Carousel.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel className='w-50' activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 container"
+          src="https://dummyimage.com/600x400/000/fff.jpg&text=reactbootstrap"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://dummyimage.com/6000x4000/000/fff.jpg&text=reactbootstrap"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://dummyimage.com/600x400/000/fff.jpg&text=reactbootstrap"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ControlledCarousel
